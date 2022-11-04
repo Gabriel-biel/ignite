704402191bd2ad3a680f7fe46aa97a0c2a159296
Gabriel-biel <gabriel97gla98@gmail.com>
2022-11-04 14:59:18 -0400

    :heavy_check_mark: project completed

---
+++ React/Ignite-Feed/src/App.tsx
@@ -0,0 +1,64 @@
+import { Header } from './Components/Header';
+import { Post } from './Components/Post';
+import { Sidebar } from './Components/Sidebar';
+
+import './global.css';
+import styles from './App.module.css'
+
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// Content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Gabriel-biel.png",
+      name: "Gabriel Andrade",
+      role: "Developer Junior"
+    },
+    content: [
+      {type: 'paragraph', content: "Fala Galera 👋🏼"},
+      {type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifolio. E um projeto que fiz durante"},
+      {type: 'link', content: "Gabriel-biel/ignite-aulas-2022"},
+    ],
+    publishedAt: new Date("2022-11-03 12:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Maik Brito",
+      role: "Educator @Rockeseat",
+    },
+    content: [
+      {type: 'paragraph', content: "Fala Galera 👋🏼"},
+      {type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifolio. E um projeto que fiz durante"},
+      {type: 'link', content: "Maik-Brito/ignite-aulas-2022"},
+    ],
+    publishedAt: new Date("2022-11-03 12:00:00"),
+  },
+]
+
+export function App() {
+  return (
+    <>
+      <Header />
+        <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+            })}
+          </main>
+        </div>
+    </>
+  )
+}
---
+++ React/Ignite-Feed/src/Components/Comment.tsx
@@ -0,0 +1,58 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+  content: string,
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeletComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false} src="https://github.com/Gabriel-biel.png" 
+        alt=''
+      />
+      
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Lima</strong>
+              <time title="30 de Outubro às 14:19" dateTime='2022-30-10 14:19:00'>Cerca de 1h atrás</time>
+            </div>
+            <button onClick={handleDeletComment} title='Deletar Comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer >
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+
+      </div>
+    </div>
+  )
+}
