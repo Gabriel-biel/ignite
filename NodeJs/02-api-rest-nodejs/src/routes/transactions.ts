27a8d063dd1794c6ed0b5ed5f6109b6c0fdc0e16
Gabriel-biel <gabriel97gla98@gmail.com>
2023-03-05 16:15:27 -0400

    :pencil:Tipagem do knex

--- NodeJs/02-api-rest-nodejs/src/routes/transactions.ts
+++ NodeJs/02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,13 +1,29 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import crypto from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
-      .returning('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
 
-    return transaction
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+    return reply.status(201).send()
+  })
+
+  app.get('/', async (request, response) => {
+    const tables = await knex('transactions').select('*').returning('*')
+    return tables
   })
 }
