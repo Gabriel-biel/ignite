5d387cc4f217ea0193521340005243fd705d0137
Gabriel-biel <gabriel97gla98@gmail.com>
2023-04-20 17:21:15 -0400

    :heavy_check_mark: Refatorando instancias nos testes

---
+++ NodeJs/03-api-solid/src/use-cases/authenticate.spec.ts
@@ -0,0 +1,58 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(inMemoryUsersRepository)
+  })
+  it('should be able to authenticate', async () => {
+    await inMemoryUsersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'jhondoe@gmail.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+  it('should not be able to authenticate with not wrong email', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(inMemoryUsersRepository)
+
+    expect(() =>
+      sut.execute({
+        email: 'jhondoe@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+  it('should not be able to authenticate with not wrong email', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(inMemoryUsersRepository)
+
+    await inMemoryUsersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    expect(() =>
+      sut.execute({
+        email: 'jhondoe@gmail.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
