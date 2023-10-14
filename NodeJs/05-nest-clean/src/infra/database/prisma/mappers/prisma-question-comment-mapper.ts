5137257f782df73eac978fdb8c18f2252c691057
Gabriel-biel <gabriel97gla98@gmail.com>
2023-10-14 17:18:28 -0400

    :heavy_check_mark: finalizando mappers

---
+++ NodeJs/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-coment'
+import { Comment as PrismaComment, Prisma } from '@prisma/client'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type.')
+    }
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        questionId: new UniqueEntityID(raw.questionId),
+        created_at: raw.createdAt,
+        updated_at: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+
+  static toPrisma(
+    QuestionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: QuestionComment.id.toString(),
+      authorId: QuestionComment.authorId.toString(),
+      questionId: QuestionComment.questionId.toString(),
+      content: QuestionComment.content,
+    }
+  }
+}
