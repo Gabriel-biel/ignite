c8ea778c3436ac6fd8a8bd19beca98f944338e33
Gabriel-biel <gabriel97gla98@gmail.com>
2023-10-25 16:49:28 -0400

    :heavy_check_mark: gerando jwt

---
+++ NodeJs/challenges/desafio-05-fastfeet/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { ConfigService } from '@nestjs/config'
+import { Env } from 'src/env'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const JWT_PRIVATE_KEY = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const JWT_PUBLIC_KEY = config.get('JWT_PUBLIC_KEY', { infer: true })
+
+        return {
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(JWT_PRIVATE_KEY, 'base64'),
+          publicKey: Buffer.from(JWT_PUBLIC_KEY, 'base64'),
+        }
+      },
+    }),
+  ],
+})
+export class AuthModule {}
